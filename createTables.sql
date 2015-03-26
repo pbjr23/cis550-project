6fcc051cf737810405d1b1e743c4b47fce9235ed
Nathaniel Chan <channat@seas.upenn.edu>
2015-03-26 17:36:24 -0400

    modified tables names and syntax errors

--- createTables.sql
+++ createTables.sql
@@ -1,22 +1,22 @@
 # DDL code to initialize schema
-CREATE TABLE user (
+CREATE TABLE users (
     username VARCHAR(20) NOT NULL PRIMARY KEY,
-    fb_id BIGINT UNSIGNED
-)
+    fb_id NUMBER
+);
 
 CREATE TABLE friends (
     user1 VARCHAR(20) NOT NULL,
     user2 VARCHAR(20) NOT NULL,
     PRIMARY KEY (user1, user2),
-    FOREIGN KEY (user1) REFERENCES user(username),
-    FOREIGN KEY (user2) REFERENCES user(username)
-)
+    FOREIGN KEY (user1) REFERENCES users(username),
+    FOREIGN KEY (user2) REFERENCES users(username)
+);
 
 CREATE TABLE password (
     username VARCHAR(20) NOT NULL PRIMARY KEY,
     pass VARCHAR(30) NOT NULL, #will be encrypted
-    FOREIGN KEY (username) REFERENCES user
-)
+    FOREIGN KEY (username) REFERENCES users
+);
 
 # weak entity, one user can save multiple addresses
 CREATE TABLE address (
@@ -24,23 +24,23 @@ CREATE TABLE address (
 	username VARCHAR(20) NOT NULL,
 	address VARCHAR(100) NOT NULL,
 	lat FLOAT(6) NOT NULL,
-	long FLOAT(6) NOT NULL,
+	lon FLOAT(6) NOT NULL,
 	PRIMARY KEY (username, address_label),
-	FOREIGN KEY (username) REFERENCES user ON DELETE CASCADE
-)
+	FOREIGN KEY (username) REFERENCES users ON DELETE CASCADE
+);
 
-CREATE TABLE group (
-    group_id INTEGER UNSIGNED NOT NULL PRIMARY KEY,
+CREATE TABLE groups (
+    group_id INTEGER NOT NULL PRIMARY KEY,
     group_name VARCHAR(20) NOT NULL
-)
+);
 
 CREATE TABLE in_group (
     group_id INTEGER NOT NULL,
     username VARCHAR(20) NOT NULL,
     PRIMARY KEY (username, group_id),
-    FOREIGN KEY group_id REFERENCES group,
-    FOREIGN KEY username REFERENCES user
-)
+    FOREIGN KEY (group_id) REFERENCES groups,
+    FOREIGN KEY (username) REFERENCES users
+);
 
 CREATE TABLE restaurant (
     rid VARCHAR(30) NOT NULL PRIMARY KEY,
@@ -48,7 +48,7 @@ CREATE TABLE restaurant (
     stars INTEGER,
     address VARCHAR(100) NOT NULL,
     lat FLOAT(6) NOT NULL,
-    long FLOAT(6) NOT NULL,
+    lon FLOAT(6) NOT NULL,
     city VARCHAR(20),
     state CHAR(2),
     review_count INTEGER,
@@ -78,21 +78,21 @@ CREATE TABLE category (
     category_name VARCHAR(20) NOT NULL,
     rid VARCHAR(30) NOT NULL,
     PRIMARY KEY (rid, category_name),
-    FOREIGN KEY rid REFERENCES restaurant
-)
+    FOREIGN KEY (rid) REFERENCES restaurant
+);
 
 CREATE TABLE group_fav (
-    group_id INTEGER UNSIGNED NOT NULL,
+    group_id INTEGER NOT NULL,
     rid VARCHAR(30) NOT NULL,
     PRIMARY KEY (group_id, rid),
-    FOREIGN KEY group_id REFERENCES group,
-    FOREIGN KEY rid REFERENCES restaurant
-)
+    FOREIGN KEY (group_id) REFERENCES groups,
+    FOREIGN KEY (rid) REFERENCES restaurant
+);
 
 CREATE TABLE user_fav (
     username VARCHAR(20) NOT NULL,
     rid VARCHAR(30) NOT NULL,
     PRIMARY KEY (username, rid),
-    FOREIGN KEY username REFERENCES user,
-    FOREIGN KEY rid REFERENCES restaurant
-)
+    FOREIGN KEY (username) REFERENCES users,
+    FOREIGN KEY (rid) REFERENCES restaurant
+);
