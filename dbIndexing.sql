20365f446b8ebacdb9f87ee43479f8417d967b56
Nathaniel Chan <channat@seas.upenn.edu>
2015-04-12 16:23:29 -0400

    added indexing to database

---
+++ dbIndexing.sql
@@ -0,0 +1,33 @@
+# indexing for restaurant table
+CREATE INDEX rest_rid_name_index
+ON restaurant (rid, name);
+
+CREATE INDEX rest_name_index
+ON restaurant (name);
+
+CREATE INDEX rest_lat_index
+ON restaurant (lat);
+
+CREATE INDEX rest_long_index
+ON restaurant (lon);
+
+# indexing for users table
+CREATE UNIQUE INDEX users_username_index
+ON users (username);
+
+# password table
+CREATE UNIQUE INDEX password_user_pass_index
+ON password (username, pass);
+
+CREATE INDEX address_latlon_index
+ON address (lat, lon);
+
+CREATE UNIQUE INDEX address_username_latlon_index
+ON address (username, lat, lon);
+
+CREATE UNIQUE INDEX groups_id_name_index
+ON groups (group_id, group_name);
+
+CREATE UNIQUE INDEX in_group_group_id
+ON in_group (group_id);
+
