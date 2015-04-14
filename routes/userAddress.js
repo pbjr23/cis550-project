fd75a208fa46617a2121e24795dc97192816dc66
Vincent <vincent.gubitosi@gmail.com>
2015-04-14 07:02:10 -0400

    Implemented getUserAddress and getUserLatLong.

---
+++ routes/userAddress.js
@@ -0,0 +1,74 @@
+// Connect string to Oracle
+var connectData = { 
+  "hostname": "yelpequidistance.cqf3bokykxqv.us-east-1.rds.amazonaws.com", 
+  "user": "equidist", 
+  "password": "helpyelp450", 
+  "database": "equiDB" };
+var oracle =  require("oracle");
+
+/////
+// Query the oracle database, and call output_actors on the results
+//
+// res = HTTP result object sent back to the client
+// name = Name to query for
+function query_db(res) {
+  oracle.connect(connectData, function(err, connection) {
+    if ( err ) {
+    	console.log(err);
+    } else {
+	  	// selecting rows
+	  	connection.execute("SELECT * FROM address WHERE rownum <= 10", 
+	  			   [], 
+	  			   function(err, results) {
+	  	    if ( err ) {
+	  	    	console.log(err);
+	  	    } else {
+	  	    	connection.close(); // done with the connection
+	  	    	console.log(results);
+//	  	    	output_actors(res, name, results);
+	  	    }
+	
+	  	}); // end connection.execute
+    }
+  }); // end oracle.connect
+}
+
+function getUserAddress(String username, callback) {
+	oracle.connect(connectData, function(err, connection) {
+		if (err) {
+			console.log(err);
+		} else {
+			connection.execute("SELECT address FROM address WHERE username = '" + username + "'", 
+			       [], function(err, results) {
+				if (err) {
+					console.log(err);
+				} else {
+					callback(results);
+				}
+			});
+		}
+	});
+}
+
+function getUserLatLong(String username, callback) {
+	oracle.connect(connectData, function(err, connection) {
+		if (err) {
+			console.log(err);
+		} else {
+			connection.execute("SELECT lat, lon FROM address WHERE username = '" + username + "'", 
+			       [], function(err, results) {
+				if (err) {
+					console.log(err);
+				} else {
+					callback(results);
+				}
+			});
+		}
+	});
+}
+
+/////
+// This is what's called by the main app 
+exports.do_work = function(req, res){
+	query_db(res,req.query.name);
+};
